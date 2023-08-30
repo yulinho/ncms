154595f9859a1534d64d28e8b555d62c55fbe708
Blake <4918605@qq.com>
2023-08-30 21:47:20 +0800

    Update install.service.js

--- core/services/install.service.js
+++ core/services/install.service.js
@@ -16,150 +16,157 @@ var hasInstall = false;
  * 查询安装状态
  * @param {Function} callback
  */
-exports.status = function (callback) {
-  if (hasInstall) {
-    return callback(null, true);
-  }
-
-  fs.stat(path.join(__dirname, '../../install.lock'), function(err, stat) {
-    if (err && err.code == 'ENOENT') {
-      return callback(null, false);
-    } else if (err) {
-      err.type = 'system';
-      return callback(err);
+exports.status = function(callback) {
+    if (hasInstall) {
+        return callback(null, true);
     }
 
-    if (stat.isFile()) {
-      hasInstall = true;
+    fs.stat(path.join(__dirname, '../../install.lock'), function(err, stat) {
+        if (err && err.code == 'ENOENT') {
+            return callback(null, false);
+        } else if (err) {
+            err.type = 'system';
+            return callback(err);
+        }
 
-      callback(null, true);
-    } else {
-      var err = {
-        type: 'system',
-        error: 'install.lock 非文件，请检查'
-      };
-      callback(err);
-    }
-  });
+        if (stat.isFile()) {
+            hasInstall = true;
+
+            callback(null, true);
+        } else {
+            var err = {
+                type: 'system',
+                error: 'install.lock 非文件，请检查'
+            };
+            callback(err);
+        }
+    });
 };
 
 /**
  * 安装
  * @param {Function} callback
  */
-exports.install = function (options, callback) {
-  if (!options.databaseDate || !options.siteInfoDate || !options.userDate) {
-    var err = {
-      type: 'system',
-      error: '没有 data 传入'
-    };
+exports.install = function(options, callback) {
+    if (!options.databaseDate || !options.siteInfoDate || !options.userDate) {
+        var err = {
+            type: 'system',
+            error: '没有 data 传入'
+        };
 
-    callback(err);
-  }
+        callback(err);
+    }
+    console.log(`---install.server.js---`, options);
+    var databaseDate = options.databaseDate;
+    var siteInfoDate = options.siteInfoDate;
+    var userDate = options.userDate;
+    // 导入示例数据，下一版本
+    //var caseDate = options.caseDate;
 
-  var databaseDate = options.databaseDate;
-  var siteInfoDate = options.siteInfoDate;
-  var userDate = options.userDate;
-  // 导入示例数据，下一版本
-  //var caseDate = options.caseDate;
+    async.auto({
+        checkInstall: function(callback) {
+            exports.status(function(err, hasInstall) {
+                if (err) return callback(err);
 
-  async.auto({
-    checkInstall: function (callback) {
-      exports.status(function (err, hasInstall) {
+                if (hasInstall) {
+                    var err = {
+                        type: 'system',
+                        error: '非法调用，ncms 已经安装'
+                    };
+
+                    return callback(err);
+                }
+
+                callback();
+            });
+        },
+        databaseInt: ['checkInstall', function(callback) {
+            database.init(databaseDate, callback)
+        }],
+        connectDatabase: ['databaseInt', database.connect],
+        writeDatabaseVersion: ['connectDatabase', function(callback) {
+            new optionsModel({
+                name: 'database',
+                value: 1
+            }).save(function(err) {
+                if (err) {
+                    err.type = 'database';
+                    return callback(err);
+                }
+
+                callback(err)
+            });
+        }],
+        writeSiteinfo: ['connectDatabase', function(callback) {
+            new optionsModel({
+                name: 'siteInfo',
+                value: siteInfoDate
+            }).save(function(err) {
+                if (err) {
+                    err.type = 'database';
+                    return callback(err);
+                }
+
+                callback();
+            });
+        }],
+        writeRole: ['connectDatabase', function(callback) {
+            new rolesModel({
+                name: '管理员',
+                description: '系统内置',
+                authorities: [100000]
+            }).save(function(err, role) {
+                if (err) {
+                    err.type = 'database';
+                    return callback(err);
+                }
+
+                callback(null, role);
+            });
+        }],
+        writeUser: ['writeRole', function(callback, results) {
+            new usersModel({
+                type: 'admin',
+                email: userDate.email,
+                nickname: userDate.nickname,
+                password: userDate.password,
+                role: results.writeRole._id
+            }).save(function(err, user) {
+                if (err) {
+                    err.type = 'database';
+                    return callback(err);
+                }
+
+                callback(null, user);
+            });
+        }],
+        writeCase: ['writeUser', function(callback) {
+            // 导入示例数据，下一版本
+            callback();
+        }],
+        writeInstallLock: ['writeCase', function(callback) {
+            try {
+                fs.writeFile('install.lock', true, function(err) {
+                    if (err) {
+                        err.type = 'system';
+                        return callback(err);
+                    }
+
+                    callback();
+                });
+            } catch (ex) {
+                console.log(`---写入install.lock失败,请注意权限---`, ex);
+                return callback(err);
+                callback();
+            }
+        }]
+    }, function(err, results) {
         if (err) return callback(err);
 
-        if (hasInstall) {
-          var err = {
-            type: 'system',
-            error: '非法调用，ncms 已经安装'
-          };
+        var data = {
+            user: results.writeUser
+        };
 
-          return callback(err);
-        }
-
-        callback();
-      });
-    },
-    databaseInt: ['checkInstall', function (callback) {
-      database.init(databaseDate, callback)
-    }],
-    connectDatabase: ['databaseInt', database.connect],
-    writeDatabaseVersion: ['connectDatabase', function (callback) {
-      new optionsModel({
-        name: 'database', value: 1
-      }).save(function (err) {
-        if (err) {
-          err.type = 'database';
-          return callback(err);
-        }
-
-        callback(err)
-      });
-    }],
-    writeSiteinfo: ['connectDatabase', function (callback) {
-      new optionsModel({
-        name: 'siteInfo',
-        value: siteInfoDate
-      }).save(function (err) {
-        if (err) {
-          err.type = 'database';
-          return callback(err);
-        }
-
-        callback();
-      });
-    }],
-    writeRole: ['connectDatabase', function (callback) {
-      new rolesModel({
-        name: '管理员',
-        description: '系统内置',
-        authorities: [100000]
-      }).save(function (err, role) {
-        if (err) {
-          err.type = 'database';
-          return callback(err);
-        }
-
-        callback(null, role);
-      });
-    }],
-    writeUser: ['writeRole', function (callback, results) {
-      new usersModel({
-        type: 'admin',
-        email: userDate.email,
-        nickname: userDate.nickname,
-        password: userDate.password,
-        role: results.writeRole._id
-      }).save(function (err, user) {
-          if (err) {
-            err.type = 'database';
-            return callback(err);
-          }
-
-        callback(null, user);
-      });
-    }],
-    writeCase: ['writeUser', function (callback) {
-      // 导入示例数据，下一版本
-      callback();
-    }],
-    writeInstallLock: ['writeCase', function (callback) {
-      fs.writeFile('install.lock', true, function (err) {
-        if (err) {
-          err.type = 'system';
-          return callback(err);
-        }
-
-        callback();
-      });
-    }]
-  }, function (err, results) {
-    if (err) return callback(err);
-
-    var data = {
-      user: results.writeUser
-    };
-
-    callback(null, data);
-  });
+        callback(null, data);
+    });
 };
